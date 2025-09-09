ce469cc572ffaace353d9dd162613cf4694780dc
rudisiona <rsmageo@gmail.com>
2025-09-08 19:35:06 -0700

    server fix

--- src/services/authServices.js
+++ src/services/authServices.js
@@ -1,5 +1,5 @@
 // src/services/authServices.js
-const BASE = import.meta.env.VITE_API_BASE_URL || "http://localhost:3001";
+const BASE = import.meta.env.VITE_API_BASE_URL || "http://localhost:3000";
 
 // token helpers
 export function getToken() {
